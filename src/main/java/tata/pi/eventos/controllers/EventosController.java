9fade215a9910ad6fae9a32ca5c13c7db7db8fec
StephanyTaina2211 <stephany.o@escolar.ifrn.edu.br>
2023-09-11 22:32:04 -0300

    Inserindo o método que mostra as informações obtidas no formulário

--- src/main/java/tata/pi/eventos/controllers/EventosController.java
+++ src/main/java/tata/pi/eventos/controllers/EventosController.java
@@ -1,7 +1,11 @@
 package tata.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tata.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -10,6 +14,24 @@ public class EventosController {
 	public String form() {
 		return "formEvento";	
 	
+	}
+	
+	@PostMapping("/submit")
+	public String submetido(Evento evento) {
+   
+        System.out.println("Dados do evento:");
+        System.out.println("Nome: " + evento.getNome());
+        System.out.println("Data: " + evento.getData());
+        System.out.println("Horário: " + evento.getHorario());
+        System.out.println("Local: " + evento.getLocal());
+       
+        return "redirect:/success";
+    }
+	
+	
+	@GetMapping("/success")
+	public String successPage() {
+		return "success";
 	}
 	
 }
